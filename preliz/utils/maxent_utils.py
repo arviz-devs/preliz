459c50541e6703e4ba911702f7f1af38e4975e62
aloctavodia <aloctavodia@gmail.com>
2022-04-04 16:58:49 +0300

    add discrete support to maxent

--- preliz/utils/maxent_utils.py
+++ preliz/utils/maxent_utils.py
@@ -5,6 +5,8 @@ def optimize(dist, init_vals, lower, upper, mass):
     def prob_bound(params, dist, lower, upper, mass):
         dist._update(*params)  # pylint: disable=protected-access
         rv_frozen = dist.rv_frozen
+        if dist.kind == "discrete":
+            lower -= 1
         cdf0 = rv_frozen.cdf(lower)
         cdf1 = rv_frozen.cdf(upper)
         loss = (cdf1 - cdf0) - mass
@@ -22,6 +24,20 @@ def optimize(dist, init_vals, lower, upper, mass):
     return opt
 
 
-def relative_error(rv_frozen, upper, lower, requiered_mass):
-    computed_mass = rv_frozen.cdf(upper) - rv_frozen.cdf(lower)
-    return (computed_mass - requiered_mass) / requiered_mass * 100
+def relative_error(dist, upper, lower, requiered_mass):
+    if dist.kind == "discrete":
+        lower -= 1
+    computed_mass = dist.rv_frozen.cdf(upper) - dist.rv_frozen.cdf(lower)
+    return abs((computed_mass - requiered_mass) / requiered_mass * 100)
+
+
+def end_points_ints(lower, upper):
+    return is_integer_num(lower) and is_integer_num(upper)
+
+
+def is_integer_num(obj):
+    if isinstance(obj, int):
+        return True
+    if isinstance(obj, float):
+        return obj.is_integer()
+    return False
