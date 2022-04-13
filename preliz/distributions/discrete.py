95f90fbc60e87c30c2245adeb278bcdc935b73d3
aloctavodia <aloctavodia@gmail.com>
2022-04-13 07:20:27 +0300

    add discrete distributions and tests

--- preliz/distributions/discrete.py
+++ preliz/distributions/discrete.py
@@ -2,9 +2,173 @@
 Discrete probability distributions.
 """
 # pylint: disable=useless-super-delegation
+import numpy as np
 from scipy import stats
 
+
 from .distributions import Discrete
+
+
+class Binomial(Discrete):
+    R"""
+    Binomial distribution.
+
+    The discrete probability distribution of the number of successes
+    in a sequence of n independent yes/no experiments, each of which
+    yields success with probability p.
+
+    The pmf of this distribution is
+
+    .. math:: f(x \mid n, p) = \binom{n}{x} p^x (1-p)^{n-x}
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import Binomial
+        az.style.use('arviz-white')
+        ns = [5, 10, 10]
+        ps = [0.5, 0.5, 0.7]
+        for n, p in zip(ns, ps):
+            Binomial(n, p).plot_pdf()
+
+    ========  ==========================================
+    Support   :math:`x \in \{0, 1, \ldots, n\}`
+    Mean      :math:`n p`
+    Variance  :math:`n p (1 - p)`
+    ========  ==========================================
+
+    Parameters
+    ----------
+    n : int
+        Number of Bernoulli trials (n >= 0).
+    p : float
+        Probability of success in each trial (0 < p < 1).
+    """
+
+    def __init__(self, n=None, p=None):
+        super().__init__()
+        self.n = n
+        self.p = p
+        self.name = "binomial"
+        self.params = (self.n, self.p)
+        self.param_names = ("n", "p")
+        self.dist = stats.binom
+        self._update_rv_frozen()
+
+    def _get_frozen(self):
+        return self.dist(self.n, self.p)
+
+    def _update(self, n, p):
+        self.n = n
+        self.p = p
+        self.params = (self.n, p)
+        self._update_rv_frozen()
+
+    def fit_moments(self, mean, sigma):
+        # check if this is a good
+        n_hat = mean**2 / (mean - sigma**2)
+        n = int(max(1, n_hat))
+        p = mean / n_hat
+        self._update(n, p)
+
+    def fit_mle(self, sample):
+        # see https://doi.org/10.1016/j.jspi.2004.02.019 for details
+        x_bar = np.mean(sample)
+        x_std = np.std(sample)
+        x_max = np.max(sample)
+        n = x_max ** (1.5) * x_std / (x_bar**0.5 * (x_max - x_bar) ** 0.5)
+        p = x_bar / n
+        self._update(n, p)
+
+    def plot_pdf(
+        self, box=False, quantiles=None, support="full", legend="legend", figsize=None, ax=None
+    ):
+        return super().plot_pdf(box, quantiles, support, legend, figsize, ax)
+
+    def plot_cdf(self, support="full", legend="legend", figsize=None, ax=None):
+        return super().plot_cdf(support, legend, figsize, ax)
+
+    def plot_ppf(self, legend="legend", figsize=None, ax=None):
+        return super().plot_ppf(legend, figsize, ax)
+
+
+class NegativeBinomial(Discrete):
+    R"""
+    Negative binomial distribution.
+
+    If it is parametrized in terms of n and p, the negative binomial describes
+    the probability to have x failures before the n-th success, given the
+    probability p of success in each trial. Its pmf is
+
+    .. math::
+
+        f(x \mid n, p) =
+           \binom{x + n - 1}{x}
+           (p)^n (1 - p)^x
+
+    .. plot::
+        :context: close-figs
+
+        import arviz as az
+        from preliz import NegativeBinomial
+        az.style.use('arviz-white')
+        ns = [5, 10, 10]
+        ps = [0.5, 0.5, 0.7]
+        for n, p in zip(ns, ps):
+            NegativeBinomial(n, p).plot_pdf()
+
+    ========  ==========================
+    Support   :math:`x \in \mathbb{N}_0`
+    Mean      :math:`\frac{(1-p) r}{p}`
+    Variance  :math:`\frac{(1-p) r}{p^2}`
+    ========  ==========================
+
+    Parameters
+    ----------
+    n: float
+        Number of target success trials (n > 0)
+    p: float
+        Probability of success in each trial (0 < p < 1).
+    """
+
+    def __init__(self, n=None, p=None):
+        super().__init__()
+        self.n = n
+        self.p = p
+        self.name = "negativebinomial"
+        self.params = (self.n, self.p)
+        self.param_names = ("n", "p")
+        self.dist = stats.nbinom
+        self._update_rv_frozen()
+
+    def _get_frozen(self):
+        return self.dist(self.n, self.p)
+
+    def _update(self, n, p):
+        self.n = n
+        self.p = p
+        self.params = (self.n, p)
+        self._update_rv_frozen()
+
+    def fit_moments(self, mean, sigma):
+        n = mean**2 / (sigma**2 - mean)
+        p = mean / sigma**2
+        self._update(n, p)
+
+    def fit_mle(self, sample):
+        raise NotImplementedError
+
+    def plot_pdf(
+        self, box=False, quantiles=None, support="full", legend="legend", figsize=None, ax=None
+    ):
+        return super().plot_pdf(box, quantiles, support, legend, figsize, ax)
+
+    def plot_cdf(self, support="full", legend="legend", figsize=None, ax=None):
+        return super().plot_cdf(support, legend, figsize, ax)
+
+    def plot_ppf(self, legend="legend", figsize=None, ax=None):
+        return super().plot_ppf(legend, figsize, ax)
 
 
 class Poisson(Discrete):
@@ -64,9 +228,9 @@ class Poisson(Discrete):
     def fit_moments(self, mean, sigma=None):  # pylint: disable=unused-argument
         self._update(mean)
 
-    def fit_mle(self, sample, **kwargs):
-        # This is not available from scipy. We will use our own implementation
-        raise NotImplementedError
+    def fit_mle(self, sample):
+        mu = np.mean(sample)
+        self._update(mu)
 
     def plot_pdf(
         self, box=False, quantiles=None, support="full", legend="legend", figsize=None, ax=None
