849e55cb3cf74f315c7926f7bc417c25d34ed64f
aloctavodia <aloctavodia@gmail.com>
2022-08-30 13:45:34 -0300

    mle improve docs

--- preliz/mle.py
+++ preliz/mle.py
@@ -9,7 +9,7 @@ _log = logging.getLogger("preliz")
 def mle(
     distributions,
     sample,
-    plot="first",
+    plot=1,
     plot_kwargs=None,
     ax=None,
 ):
@@ -20,13 +20,21 @@ def mle(
     Parameters
     ----------
     distributions : list of PreliZ distribution
-        Instance of a PreliZ distribution
+        Instance of a PreliZ distribution. Notice that the distributions will be
+        updated inplace.
     sample : list or 1D array-like
+        Data used to estimate the distribution parameters.
+    plot : int
+        Number of distributions to plots. Defaults to ``1`` (i.e. plot the best match)
+        If larger than the number of passed distributions it will plot all of them.
+        Use ``0`` or ``False`` to disable plotting.
+    plot_kwargs : dict
+        Dictionary passed to the method ``plot_pdf()`` of ``distribution``.
+    ax : matplotlib axes
 
     Returns
     -------
-    PreliZ distribution
-
+    axes: matplotlib axes
     """
     sample = np.array(sample)
     x_min = sample.min()
@@ -34,10 +42,12 @@ def mle(
 
     fitted = fit_to_sample(distributions, sample, x_min, x_max)
 
+    if plot > len(distributions):
+        plot = len(distributions)
+
     if plot:
         idx = np.argsort(fitted.losses)
-        if plot == "first":
-            idx = idx[:1]
+        idx = idx[:plot]
         for dist in fitted.distributions[idx]:
             if dist is not None:
                 ax = dist.plot_pdf(plot_kwargs)
