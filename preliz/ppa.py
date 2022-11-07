8c2c33c550b06369a184636c4c3a13582eb938f6
Osvaldo A Martin <aloctavodia@gmail.com>
2022-11-07 10:50:21 -0300

    Prior predictive check assistant (#89)
    * draft new version
    
    * add buttons
    
    * update
    
    * reset clicked
    
    * clean
    
    * remove scikit-learn as a requirement

--- preliz/ppa.py
+++ preliz/ppa.py
@@ -2,12 +2,12 @@
 
 import logging
 
-import matplotlib.pyplot as plt
-
 import arviz as az
+import ipywidgets as widgets
+import matplotlib.pyplot as plt
 import numpy as np
-import sklearn.cluster as clu
-from sklearn.preprocessing import StandardScaler
+from scipy.spatial import KDTree
+
 
 from preliz import distributions
 from .utils.plot_utils import plot_pointinterval, repr_to_matplotlib
@@ -20,7 +20,7 @@ pymc_to_preliz = {
 }
 
 
-def ppa(idata, model, prepros="octiles", method="affinity", random_seed=None, backfitting=True):
+def ppa(idata, model, summary="octiles"):
     """
     Prior predictive check assistant.
 
@@ -30,164 +30,241 @@ def ppa(idata, model, prepros="octiles", method="affinity", random_seed=None, ba
     ----------
 
     idata : InferenceData
-        With samples from the prior and prior predictive distributions
+        With at least the `prior` and `prior_predictive` groups
     model : PyMC model
-        Model associated to ``ìdata``.
-    summary : str
-        summary statistic using for clustering
-    method : str
-        clustering method
-    random_seed : int
-        random seed passed to the clustering method
-    backfitting : bool
-        This is not doing anything at this point
+        Model associated to ``idata``.
+    summary : str:
+        Summary statistics applied to prior samples in order to define (dis)similarity
+        of distributions. Current options are `octiles`, `hexiles`, `quantiles`,
+        `sort` (sort data) `octi_sum` (robust estimation of first 4 moments from octiles).
     """
     _log.info(
         """Enter at your own risk."""
         """This is highly experimental code and not recommended for regular use."""
     )
+
+    try:
+        shell = get_ipython().__class__.__name__  # pylint:disable=undefined-variable
+        if shell == "ZMQInteractiveShell" and "nbagg" not in get_backend():
+            _log.info(
+                "To run roulette you need Jupyter notebook, or Jupyter lab."
+                "You will also need to use the magic `%matplotlib widget`"
+            )
+    except NameError:
+        pass
+
+    shown = []
     obs_rv = model.observed_RVs[0].name  # only one observed variable for the moment
     pp_samples = idata.prior_predictive[obs_rv].squeeze().values
     prior_samples = idata.prior.squeeze()
     sample_size = pp_samples.shape[0]
-    db0 = clusterize(pp_samples, prepros, method, random_seed)
-    fig, _ = plot_clusters(pp_samples, db0)
+    pp_summary, kdt = compute_summaries(pp_samples, summary)
+    pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt)
+    fig, axes = plot_samples(pp_samples, pp_samples_idxs)
 
     clicked = []
+    selected = []
+    choices = []
 
-    def onclick(event):
-        if event.inaxes not in clicked:
-            clicked.append(event.inaxes)
-        else:
-            clicked.remove(event.inaxes)
-            plt.setp(event.inaxes.spines.values(), color="k", lw=1)
+    output = widgets.Output()
 
-        for ax in clicked:
-            plt.setp(ax.spines.values(), color="C1", lw=3)
+    with output:
+        button_carry_on = widgets.Button(description="carry on")
+        button_return_prior = widgets.Button(description="return prior")
 
-    def on_leave_fig(event):  # pylint: disable=unused-argument
-        if clicked:
-            choices = [int(ax.get_title()) for ax in clicked]
-            pps1 = resample(choices, prior_samples, model, db0)
-            if backfitting:
-                string = new_priors_back_fitting(model, pps1, sample_size)
-            # else:
-            #     string = new_priors_posterior(model, pps1, sample_size)
+        def carry_on_(_):
+            carry_on(fig, axes, clicked, pp_samples, pp_summary, choices, selected, shown, kdt)
 
-            fig.clf()
-            plt.text(0.2, 0.5, string, fontsize=14)
-            plt.yticks([])
-            plt.xticks([])
+        button_carry_on.on_click(carry_on_)
 
-    fig.canvas.mpl_connect("button_press_event", onclick)
-    fig.canvas.mpl_connect("figure_leave_event", on_leave_fig)
+        def on_return_prior_(_):
+            on_return_prior(fig, selected, model, sample_size)
+
+        button_return_prior.on_click(on_return_prior_)
+
+        def click(event):
+            if event.inaxes not in clicked:
+                clicked.append(event.inaxes)
+            else:
+                clicked.remove(event.inaxes)
+                plt.setp(event.inaxes.spines.values(), color="k", lw=1)
+
+            for ax in clicked:
+                plt.setp(ax.spines.values(), color="C1", lw=3)
+            fig.canvas.draw()
+
+        def on_return_prior(fig, selected, model, sample_size):
+            selected = np.unique(selected)
+            num_selected = len(selected)
+
+            if num_selected > 1:
+                string = (
+                    f"You have selected {num_selected} out of {sample_size} prior"
+                    "predictive samples\nThat selection is consistent with the"
+                    "following priors:\n"
+                )
+
+                subsample = select_prior_samples(selected, prior_samples, model)
+                string = back_fitting(model, subsample, string)
+
+                fig.clf()
+                plt.text(0.2, 0.5, string, fontsize=14)
+                plt.yticks([])
+                plt.xticks([])
+            else:
+                fig.suptitle("Please select more distributions", fontsize=16)
+
+            fig.canvas.draw()
+
+        fig.canvas.mpl_connect("button_press_event", click)
+
+    controls = widgets.VBox([button_carry_on, button_return_prior])
+
+    display(widgets.HBox([controls]))  # pylint:disable=undefined-variable
 
 
-def clusterize(prior_predictive_samples, prepros="octiles", method="kmeans", random_seed=None):
-    if prepros is None:
-        pp_samples = prior_predictive_samples
+def carry_on(fig, axes, clicked, pp_samples, pp_summary, choices, selected, shown, kdt):
+    choices.extend([int(ax.get_title()) for ax in clicked])
+    selected.extend(choices)
+
+    fig.suptitle("")
+    for ax in clicked:
+        plt.setp(ax.spines.values(), color="k", lw=1)
+    for ax in axes:
+        ax.cla()
+    for ax in list(clicked):
+        clicked.remove(ax)
+
+    pp_samples_idxs, shown = keep_sampling(pp_summary, choices, shown, kdt)
+    if not pp_samples_idxs:
+        pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt)
+    fig, _ = plot_samples(pp_samples, pp_samples_idxs, fig)
+    fig.canvas.draw()
+
+
+def compute_summaries(pp_samples, summary):
+    if summary == "octiles":
+        pp_summary = np.quantile(
+            pp_samples, [0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], axis=1
+        ).T
+    elif summary == "quartiles":
+        pp_summary = np.quantile(
+            pp_samples,
+            [
+                0.25,
+                0.5,
+                0.75,
+            ],
+            axis=1,
+        ).T
+
+    elif summary == "hexiles":
+        pp_summary = np.quantile(pp_samples, [0.25, 0.375, 0.5, 0.625, 0.75], axis=1).T
+    elif summary == "sort":
+        pp_summary = np.sort(pp_samples)
+    elif summary == "octi_sum":
+        suma = np.quantile(pp_samples, [0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], axis=1).T
+        s_a = suma[:, 3]
+        s_b = suma[:, 5] - suma[:, 1]
+        s_g = (suma[:, 5] + suma[:, 1] - 2 * suma[:, 3]) / s_b
+        s_k = (suma[:, 6] - suma[:, 4] + suma[:, 2] - suma[:, 0]) / s_b
+        pp_summary = np.stack([s_a, s_b, s_g, s_k]).T
+
+    kdt = KDTree(pp_summary)
+    return pp_summary, kdt
+
+
+def initialize_subsamples(pp_summary, shown, kdt):
+    new = np.random.choice(list(set(range(0, len(pp_summary))) - set(shown)))
+    samples = [new]
+
+    for _ in range(8):
+        farthest_neighbor = pp_summary.shape[0]
+        while new in samples:
+            _, new = kdt.query(pp_summary[samples[-1]], [farthest_neighbor])
+            new = new.item()
+            farthest_neighbor -= 1
+        samples.append(new)
+
+    shown.extend(samples)
+
+    return samples, shown
+
+
+def keep_sampling(pp_summary, choices, shown, kdt):
+    """
+    Find distribution similar to the ones in `choices`, but not already shown.
+    If `choices` is empty return an empty selection.
+    """
+    if choices:
+        new = choices.pop(0)
+        samples = [new]
+
+        for _ in range(9):
+            nearest_neighbor = 2
+            while new in samples or new in shown:
+                _, new = kdt.query(pp_summary[samples[-1]], [nearest_neighbor])
+                new = new.item()
+                nearest_neighbor += 1
+            samples.append(new)
+
+        shown.extend(samples[1:])
+
+        return samples[1:], shown
     else:
-        if prepros == "octiles":
-            pp_samples = np.quantile(
-                prior_predictive_samples, [0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], axis=1
-            ).T
-        elif prepros == "quartiles":
-            pp_samples = np.quantile(
-                prior_predictive_samples,
-                [
-                    0.25,
-                    0.5,
-                    0.75,
-                ],
-                axis=1,
-            ).T
+        return [], shown
 
-        elif prepros == "hexiles":
-            pp_samples = np.quantile(
-                prior_predictive_samples, [0.25, 0.375, 0.5, 0.625, 0.75], axis=1
-            ).T
-        elif prepros == "sort":
-            pp_samples = np.sort(prior_predictive_samples)
 
-    if method == "kmeans":
-        clust = clu.KMeans(n_clusters=9, random_state=random_seed).fit(pp_samples)
-    elif method == "spectral":
-        pp_samples = StandardScaler().fit_transform(pp_samples)
-        clust = clu.SpectralClustering(n_clusters=9, random_state=random_seed).fit(pp_samples)
-    elif method == "affinity":
-        pp_samples = StandardScaler().fit_transform(pp_samples)
-        clust = clu.AffinityPropagation(random_state=random_seed).fit(pp_samples)
+def plot_samples(pp_samples, pp_samples_idxs, fig=None):
+    row_colum = int(np.ceil(len(pp_samples_idxs) ** 0.5))
+
+    if fig is None:
+        fig, axes = plt.subplots(row_colum, row_colum, figsize=(8, 6), sharex=True)
     else:
-        raise NotImplementedError(f"The method {method} is not implemented")
+        axes = np.array(fig.axes)
 
-    return clust
-
-
-def plot_clusters(prior_predictive_samples, clust):
-    clusters = np.unique(clust.labels_)
-    clusters = clusters[clusters != -1]
-    row_colum = int(np.ceil(len(clusters) ** 0.5))
-
-    fig, axes = plt.subplots(row_colum, row_colum, figsize=(8, 6), sharex=True)
     try:
         axes = axes.ravel()
     except AttributeError:
         axes = [axes]
 
-    for ax, cluster in zip(axes, clusters):
+    for ax, idx in zip(axes, pp_samples_idxs):
         ax.axvline(0, ls="--", color="0.5")
 
-        sample = prior_predictive_samples[clust.labels_ == cluster]
-        # for s in sample:
-        #    az.plot_kde(s, ax=ax)
+        sample = pp_samples[idx]
         az.plot_kde(sample, ax=ax, plot_kwargs={"color": "C0"})
 
-        plot_pointinterval(sample, None, ax)
-        ax.set_title(cluster)
+        plot_pointinterval(sample, ax=ax)
+        ax.set_title(idx)
         ax.set_yticks([])
 
-    for idx in range(len(clusters), len(axes)):
-        axes[idx].remove()
     return fig, axes
 
 
-def resample(choices, prior_samples, model, clust):
+def select_prior_samples(selected, prior_samples, model):
+    """
+    Given a selected set of prior predictive samples pick the corresponding
+    prior samples.
+    """
     rvs = model.unobserved_RVs  # we should exclude deterministics
-    rvs_names = [rv.name for rv in rvs]
-    dado = {}
-    for rvn in rvs_names:
-        lista = []
-        for choice in choices:
-            lista.extend(prior_samples[rvn].values[clust.labels_ == choice])
-        dado[rvn] = np.array(lista)
+    rv_names = [rv.name for rv in rvs]
+    subsample = {rv: prior_samples[rv].sel(draw=selected).values.squeeze() for rv in rv_names}
 
-    # posti = pm.sample_posterior_predictive(az.from_dict(dado), model=model)
-    # dado.update(posti)
-    return dado
+    return subsample
 
 
-def new_priors_back_fitting(model, pps1, sample_size):
-    subset = len(pps1[list(pps1.keys())[0]])
-
-    string = f"You have selected {subset} out of {sample_size} prior predictive samples\n"
-    string += "That selection is consistent with the following priors:\n"
-
+def back_fitting(model, subset, string):
+    """
+    Use MLE to fit a subset of the prior samples to the individual prior distributions
+    """
     for unobserved in model.unobserved_RVs:
         distribution = unobserved.owner.op.name
         dist = getattr(distributions, pymc_to_preliz[distribution])()
-        # pymc_name, sp_rv = pymc_to_scipy[distribution]
         name = unobserved.name
         idx = unobserved.owner.nin - 3
         params = unobserved.owner.inputs[-idx:]
         is_fitable = any(param.name is None for param in params)
         if is_fitable:
-            # pymc_name, sp_rv = pymc_to_scipy[distribution]
-            dist._fit_mle(pps1[name])
+            dist._fit_mle(subset[name])
             string += f"{repr_to_matplotlib(dist)}\n"
-            # string += f'{name} = pm.{pymc_name}("{name}", '
-            # if distribution == "normal":
-            #    string += f"{prior[0]:.2f}, {prior[1]:.2f})\n"
-            # elif distribution == "halfnormal":
-            #    string += f"{prior[1]:.2f})\n"
-
     return string
