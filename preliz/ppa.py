28e7b5d11647e9056e327112019c4fbb310eb3b9
Osvaldo A Martin <aloctavodia@gmail.com>
2022-12-09 15:07:40 -0300

    ppa: allow non-random initialization (#124)
    * ppa allow non-random initiliazation
    
    * rename argument

--- preliz/ppa.py
+++ preliz/ppa.py
@@ -11,11 +11,13 @@ from scipy.spatial import KDTree
 
 from .utils.plot_utils import plot_pointinterval, repr_to_matplotlib
 from .utils.utils import get_pymc_to_preliz
+from .distributions.continuous import Normal
+from .distributions.distributions import Distribution
 
 _log = logging.getLogger("preliz")
 
 
-def ppa(idata, model, summary="octiles"):
+def ppa(idata, model, summary="octiles", init=None):
     """
     Prior predictive check assistant.
 
@@ -32,6 +34,10 @@ def ppa(idata, model, summary="octiles"):
         Summary statistics applied to prior samples in order to define (dis)similarity
         of distributions. Current options are `octiles`, `hexiles`, `quantiles`,
         `sort` (sort data) `octi_sum` (robust estimation of first 4 moments from octiles).
+    init : tuple or PreliZ distribtuion
+        Initial distribution. The first shown distributions will be selected to be as close
+        as possible to `init`. Available options are, a PreliZ distribution or a 2-tuple with
+        the first element representing the mean and the second the standard deviation.
     """
     _log.info(
         """Enter at your own risk. """
@@ -42,7 +48,7 @@ def ppa(idata, model, summary="octiles"):
         shell = get_ipython().__class__.__name__  # pylint:disable=undefined-variable
         if shell == "ZMQInteractiveShell" and "nbagg" not in get_backend():
             _log.info(
-                "To run roulette you need Jupyter notebook, or Jupyter lab."
+                "To run ppa you need Jupyter notebook, or Jupyter lab."
                 "You will also need to use the magic `%matplotlib widget`"
             )
     except NameError:
@@ -52,9 +58,12 @@ def ppa(idata, model, summary="octiles"):
     obs_rv = model.observed_RVs[0].name  # only one observed variable for the moment
     pp_samples = idata.prior_predictive[obs_rv].squeeze().values
     prior_samples = idata.prior.squeeze()
+    if init is not None:
+        pp_samples = add_init_dist(init, pp_samples)
+
     sample_size = pp_samples.shape[0]
     pp_summary, kdt = compute_summaries(pp_samples, summary)
-    pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt)
+    pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt, init)
     fig, axes = plot_samples(pp_samples, pp_samples_idxs)
 
     clicked = []
@@ -132,7 +141,7 @@ def carry_on(fig, axes, clicked, pp_samples, pp_summary, choices, selected, show
 
     pp_samples_idxs, shown = keep_sampling(pp_summary, choices, shown, kdt)
     if not pp_samples_idxs:
-        pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt)
+        pp_samples_idxs, shown = initialize_subsamples(pp_summary, shown, kdt, None)
     fig, _ = plot_samples(pp_samples, pp_samples_idxs, fig)
     fig.canvas.draw()
 
@@ -169,19 +178,44 @@ def compute_summaries(pp_samples, summary):
     return pp_summary, kdt
 
 
-def initialize_subsamples(pp_summary, shown, kdt):
-    new = np.random.choice(list(set(range(0, len(pp_summary))) - set(shown)))
-    samples = [new]
+def add_init_dist(ref_dist, pp_samples):
+    sample_size = pp_samples.shape[1]
+    if isinstance(ref_dist, tuple):
+        ref_sample = Normal(*ref_dist).rvs(sample_size)
+    elif isinstance(ref_dist, Distribution):
+        ref_sample = ref_dist.rvs(sample_size)
 
-    for _ in range(8):
-        farthest_neighbor = pp_summary.shape[0]
-        while new in samples:
-            _, new = kdt.query(pp_summary[samples[-1]], [farthest_neighbor])
-            new = new.item()
-            farthest_neighbor -= 1
-        samples.append(new)
+    pp_samples = np.vstack([ref_sample, pp_samples])
+    return pp_samples
 
-    shown.extend(samples)
+
+def initialize_subsamples(pp_summary, shown, kdt, ref_dist):
+    if ref_dist is None:
+        new = np.random.choice(list(set(range(0, len(pp_summary))) - set(shown)))
+        samples = [new]
+
+        for _ in range(8):
+            farthest_neighbor = pp_summary.shape[0]
+            while new in samples or new in shown:
+                _, new = kdt.query(pp_summary[samples[-1]], [farthest_neighbor])
+                new = new.item()
+                farthest_neighbor -= 1
+            samples.append(new)
+        shown.extend(samples)
+    else:
+        new = 0
+        samples = [new]
+
+        for _ in range(9):
+            nearest_neighbor = 2
+            while new in samples:
+                _, new = kdt.query(pp_summary[samples[-1]], [nearest_neighbor])
+                new = new.item()
+                nearest_neighbor += 1
+            samples.append(new)
+
+        samples = samples[1:]
+        shown.extend(samples)
 
     return samples, shown
 
@@ -227,7 +261,7 @@ def plot_samples(pp_samples, pp_samples_idxs, fig=None):
         ax.axvline(0, ls="--", color="0.5")
 
         sample = pp_samples[idx]
-        az.plot_kde(sample, ax=ax, plot_kwargs={"color": "C0"})
+        az.plot_kde(sample, ax=ax, plot_kwargs={"color": "C0"})  # pylint:disable=no-member
 
         plot_pointinterval(sample, ax=ax)
         ax.set_title(idx)
