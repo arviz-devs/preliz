d0cd62091b2cf94e66f2bd40084c2bacda258050
Osvaldo A Martin <aloctavodia@gmail.com>
2023-10-31 15:56:16 -0300

    refactor improve internal doc (#296)

--- preliz/predictive/ppa.py
+++ preliz/predictive/ppa.py
@@ -77,22 +77,14 @@ def ppa(fmodel, draws=2000, references=0, boundaries=(-np.inf, np.inf), target=N
             lambda event: filter_dists.carry_on(radio_buttons_kind.value, check_button_sharex.value)
         )
 
-        def on_return_prior_(_):
-            on_return_prior(
-                filter_dists.fig,
-                filter_dists.selected,
-                filter_dists.model,
-                filter_dists.prior_samples,
-            )
-
-        button_return_prior.on_click(on_return_prior_)
+        button_return_prior.on_click(lambda event: filter_dists.on_return_prior())
 
         def kind_(_):
             kind = radio_buttons_kind.value
 
             plot_pp_samples(
                 filter_dists.pp_samples,
-                filter_dists.pp_samples_idxs,
+                filter_dists.display_pp_idxs,
                 references,
                 kind,
                 check_button_sharex.value,
@@ -115,25 +107,6 @@ def ppa(fmodel, draws=2000, references=0, boundaries=(-np.inf, np.inf), target=N
                     plt.setp(ax.spines.values(), color="C1", lw=3)
                 filter_dists.fig.canvas.draw()
 
-        def on_return_prior(fig, selected, model, prior_samples):
-
-            selected = list(selected)
-
-            if len(selected) > 4:
-                subsample = select_prior_samples(selected, prior_samples, model)
-
-                string, _ = back_fitting(model, subsample, new_families=False)
-
-                fig.clf()
-                plt.text(0.05, 0.5, string, fontsize=14)
-
-                plt.yticks([])
-                plt.xticks([])
-            else:
-                fig.suptitle("Please select more distributions", fontsize=16)
-
-            fig.canvas.draw()
-
         filter_dists.fig.canvas.mpl_connect("button_press_event", click)
 
     controls = widgets.VBox([button_carry_on, button_return_prior])
@@ -146,42 +119,44 @@ def ppa(fmodel, draws=2000, references=0, boundaries=(-np.inf, np.inf), target=N
 class FilterDistribution:  # pylint:disable=too-many-instance-attributes
     def __init__(self, fmodel, draws, references, boundaries, target):
         self.fmodel = fmodel
-        self.source, _ = inspect_source(fmodel)
+        self.source = ""  # string representation of the model
         self.draws = draws
         self.references = references
         self.boundaries = boundaries
         self.target = target
-        self.pp_samples = None
-        self.prior_samples = None
-        self.pp_samples_idxs = None
-        self.pp_summary = None
-        self.obs_rv = None
-        self.fig = None
-        self.choices = []
-        self.clicked = []
-        self.selected = set()
-        self.collected_distances = {}
-        self.model = None
-        self.shown = None
-        self.distances = None
-        self.fig_pp_mean = None
-        self.axes = None
-        self.kdt = None
+        self.pp_samples = None  # prior predictive samples
+        self.prior_samples = None  # prior samples used for backfitting
+        self.display_pp_idxs = None  # indices of the pp_samples to be displayed
+        self.pp_octiles = None  # octiles computed from pp_samples
+        self.kdt = None  # KDTree used to find similar distributions
+        self.obs_rv = None  # name of the observed random variable
+        self.model = None  # parsed model used for backfitting
+        self.clicked = []  # axes clicked by the user
+        self.choices = []  # indices of the pp_samples selected by the user and not yet used to
+        # find new distributions, this list can increase or decrease in size
+        self.selected = set()  # indices of all the pp_samples selected by the user + machine
+        # this set can only increase in size
+        self.distances = {}  # distances between as selected distribution and its nearest neighbor
+        self.shown = set()  # keep track to avoid showing the same distribution twice.
+        self.fig = None  # figure used to display the pp_samples
+        self.fig_pp_mean = None  # figure used to display the mean of the pp_samples
+        self.axes = None  # axes used to display the pp_samples
 
     def __call__(self):
+
         self.pp_samples, self.prior_samples, self.obs_rv = get_prior_pp_samples(
             self.fmodel, self.draws
         )
-
+        self.source, _ = inspect_source(self.fmodel)
         self.model = parse_function_for_ppa(self.source, self.obs_rv)
 
         if self.target is not None:
             self.add_target_dist()
 
-        self.pp_summary, self.kdt = self.compute_summaries()
-        self.pp_samples_idxs, self.distances, self.shown = self.initialize_subsamples(self.target)
+        self.pp_octiles, self.kdt = self.compute_octiles()
+        self.display_pp_idxs = self.initialize_subsamples(self.target)
         self.fig, self.axes = plot_pp_samples(
-            self.pp_samples, self.pp_samples_idxs, self.references
+            self.pp_samples, self.display_pp_idxs, self.references
         )
         self.fig_pp_mean = plot_pp_mean(self.pp_samples, self.selected, self.references)
 
@@ -193,80 +168,162 @@ class FilterDistribution:  # pylint:disable=too-many-instance-attributes
 
         self.pp_samples = np.vstack([ref_sample, self.pp_samples])
 
-    def compute_summaries(self):
-        pp_summary = np.quantile(
+    def compute_octiles(self):
+        """
+        Compute the octiles for the prior predictive samples. This is used to find
+        similar distributions using a KDTree.
+
+        We have empirically found that octiles are a good choice, but this could
+        be the consequence of limited testing.
+        """
+        pp_octiles = np.quantile(
             self.pp_samples, [0.125, 0.25, 0.375, 0.5, 0.625, 0.75, 0.875], axis=1
         ).T
-        kdt = KDTree(pp_summary)
-        return pp_summary, kdt
+        kdt = KDTree(pp_octiles)
+        return pp_octiles, kdt
 
     def initialize_subsamples(self, target):
-        samples = []
-        distances = {}
-        shown = []
+        """
+        Initialize the subsamples to be displayed.
+
+        If `target` is None, we search for the farthest_neighbor (this increases diversity)
+        otherwise we search for the nearest_neighbor of target
+
+        The initialization takes into account the boundaries provided by the user.
+
+        Updates the `shown` set (already shown pp_samples) and if `target` is None
+        also updates the  `distances` dictionary.
+        """
+        pp_idxs_to_display = []
+
+        shown_list = []
         for idx, sample in enumerate(self.pp_samples):
             if np.min(sample) < self.boundaries[0] or np.max(sample) > self.boundaries[1]:
-                shown.append(idx)
+                shown_list.append(idx)
 
-        shown = set(shown)
+        self.shown.update(shown_list)
 
-        if len(shown) != self.draws:
+        # If we have not seen all the samples yet, we collect more
+        if len(self.shown) != self.draws:
             if target is None:
-                new = np.random.choice(list(set(range(0, len(self.pp_summary))) - shown))
-                samples.append(new)
+                new = np.random.choice(list(set(range(0, len(self.pp_octiles))) - self.shown))
+                pp_idxs_to_display.append(new)
 
                 for _ in range(8):
                     farthest_neighbor = self.draws
-                    while new in samples or new in shown:
-                        # we search for the farthest_neighbor
+                    while new in pp_idxs_to_display or new in self.shown:
                         _, new = self.kdt.query(
-                            self.pp_summary[samples[-1]], [farthest_neighbor], workers=-1
+                            self.pp_octiles[pp_idxs_to_display[-1]], [farthest_neighbor], workers=-1
                         )
                         new = new.item()
                         farthest_neighbor -= 1
                     # Missing neighbors are indicated with index==sample_size
                     if new != self.draws:
-                        samples.append(new)
+                        pp_idxs_to_display.append(new)
             else:
                 new = 0
-                samples.append(new)
+                pp_idxs_to_display.append(new)
 
                 for _ in range(9):
                     nearest_neighbor = 2
-                    while new in samples:
+                    while new in pp_idxs_to_display:
                         distance, new = self.kdt.query(
-                            self.pp_summary[samples[-1]], [nearest_neighbor], workers=-1
+                            self.pp_octiles[pp_idxs_to_display[-1]], [nearest_neighbor], workers=-1
                         )
                         new = new.item()
                         nearest_neighbor += 1
 
                     if new != self.draws:
-                        samples.append(new)
-                        distances[new] = distance.item()
+                        pp_idxs_to_display.append(new)
+                        self.distances[new] = distance.item()
 
-                samples = samples[1:]
+                pp_idxs_to_display = pp_idxs_to_display[1:]
 
-            shown.update(samples)
+            self.shown.update(pp_idxs_to_display)
 
-        return samples, distances, shown
+        return pp_idxs_to_display
+
+    def keep_sampling(self):
+        """
+        Find distribution similar to the ones in `choices`, but not already shown.
+        If `choices` is empty return an empty selection.
+        """
+        if self.choices:
+            new = self.choices.pop(0)
+            samples = [new]
+
+            for _ in range(9):
+                nearest_neighbor = 2
+                while new in samples or new in self.shown:
+                    distance, new = self.kdt.query(
+                        self.pp_octiles[samples[-1]], [nearest_neighbor], workers=-1
+                    )
+                    new = new.item()
+                    nearest_neighbor += 1
+
+                # Missing neighbors are indicated with index==self.draws
+                if new != self.draws:
+                    self.distances[new] = distance.item()
+                    samples.append(new)
+
+            self.shown.update(samples[1:])
+
+            return samples[1:]
+        else:
+            return []
+
+    def collect_more_samples(self):
+        """
+        Automatically extend the set of user selected distributions
+
+        If the user has selected at least two distributions we automatically extend the selection
+        by adding all the distributions that are close to the selected ones. To do so we use
+        compute a max_dist, which is the trimmed mean of the distances between the already selected
+        distributions. The trimmed mean is computed by discarding the lower and upper 10%.
+        """
+        selected_distances = np.array([v for k, v in self.distances.items() if k in self.selected])
+
+        if len(selected_distances) > 2:
+            q_r = np.quantile(selected_distances, [0.1, 0.9])
+            max_dist = np.mean(
+                selected_distances[(selected_distances > q_r[0]) & (selected_distances < q_r[1])]
+            )
+            upper = self.draws
+        else:
+            max_dist = np.inf
+            upper = 3
+
+        _, new = self.kdt.query(
+            self.pp_octiles[list(self.selected)],
+            range(2, upper),
+            distance_upper_bound=max_dist,
+            workers=-1,
+        )
+        new = new[new < self.draws].tolist()
+
+        if np.any(np.isfinite(self.boundaries)):
+            new_ = []
+            for n_s in new:
+                sample = self.pp_samples[n_s]
+                if np.min(sample) > self.boundaries[0] and np.max(sample) < self.boundaries[1]:
+                    new_.append(n_s)
+            new = new_
+
+        if new:
+            self.selected.update(new)
+            self.shown.update(new)
 
     def carry_on(self, kind, sharex):
+        """
+        Collect user input and update the plot.
+        """
         self.fig.suptitle("")
 
         if self.clicked:
             self.choices.extend([int(ax.get_title()) for ax in self.clicked])
             shuffle(self.choices)
             self.selected.update(self.choices)
-            self.selected, self.shown = collect_more_samples(
-                self.selected,
-                self.collected_distances,
-                self.shown,
-                self.pp_summary,
-                self.pp_samples,
-                self.draws,
-                self.boundaries,
-                self.kdt,
-            )
+            self.collect_more_samples()
 
             for ax in self.clicked:
                 plt.setp(ax.spines.values(), color="k", lw=1)
@@ -275,19 +332,17 @@ class FilterDistribution:  # pylint:disable=too-many-instance-attributes
             for ax in list(self.clicked):
                 self.clicked.remove(ax)
 
-        self.pp_samples_idxs, self.distances, self.shown = keep_sampling(
-            self.pp_summary, self.choices, self.shown, self.draws, self.kdt
-        )
+        self.display_pp_idxs = self.keep_sampling()
 
-        if not self.pp_samples_idxs:
-            self.pp_samples_idxs, self.distances, self.shown = self.initialize_subsamples(None)
+        # if there is nothing to show initialize a new set of samples
+        if not self.display_pp_idxs:
+            self.display_pp_idxs = self.initialize_subsamples(None)
 
-        self.collected_distances.update(self.distances)
         plot_pp_mean(self.pp_samples, list(self.selected), self.references, kind, self.fig_pp_mean)
 
-        if self.pp_samples_idxs:
+        if self.display_pp_idxs:
             plot_pp_samples(
-                self.pp_samples, self.pp_samples_idxs, self.references, kind, sharex, self.fig
+                self.pp_samples, self.display_pp_idxs, self.references, kind, sharex, self.fig
             )
         else:
             # Instead of showing this message, we should resample.
@@ -295,72 +350,21 @@ class FilterDistribution:  # pylint:disable=too-many-instance-attributes
             self.fig.suptitle("We have seen all the samples", fontsize=16)
             self.fig.canvas.draw()
 
+    def on_return_prior(self):
 
-def keep_sampling(pp_summary, choices, shown, sample_size, kdt):
-    """
-    Find distribution similar to the ones in `choices`, but not already shown.
-    If `choices` is empty return an empty selection.
-    """
-    if choices:
-        new = choices.pop(0)
-        samples = [new]
-        distances = {}
+        selected = list(self.selected)
 
-        for _ in range(9):
-            nearest_neighbor = 2
-            while new in samples or new in shown:
-                distance, new = kdt.query(pp_summary[samples[-1]], [nearest_neighbor], workers=-1)
-                new = new.item()
-                nearest_neighbor += 1
+        if len(selected) > 4:
+            subsample = select_prior_samples(selected, self.prior_samples, self.model)
 
-            # Missing neighbors are indicated with index==sample_size
-            if new != sample_size:
-                distances[new] = distance.item()
-                samples.append(new)
+            string, _ = back_fitting(self.model, subsample, new_families=False)
 
-        shown.update(samples[1:])
+            self.fig.clf()
+            plt.text(0.05, 0.5, string, fontsize=14)
 
-        return samples[1:], distances, shown
-    else:
-        return [], [], shown
+            plt.yticks([])
+            plt.xticks([])
+        else:
+            self.fig.suptitle("Please select more distributions", fontsize=16)
 
-
-def collect_more_samples(
-    selected, collected_distances, shown, pp_summary, pp_samples, sample_size, boundaries, kdt
-):
-    """
-    Automatically extend the user selected distributions
-
-    Lot of room for improving this function
-    """
-    selected_distances = np.array([v for k, v in collected_distances.items() if k in selected])
-
-    if len(selected_distances) > 2:
-        q_r = np.quantile(selected_distances, [0.1, 0.9])
-        max_dist = np.mean(
-            selected_distances[(selected_distances > q_r[0]) & (selected_distances < q_r[1])]
-        )
-        upper = sample_size
-    else:
-        max_dist = np.inf
-        upper = 3
-
-    _, new = kdt.query(
-        pp_summary[list(selected)], range(2, upper), distance_upper_bound=max_dist, workers=-1
-    )
-    new = new[new < sample_size].tolist()
-
-    if np.any(np.isfinite(boundaries)):
-        new_ = []
-        for n_s in new:
-            sample = pp_samples[n_s]
-            if np.min(sample) > boundaries[0] and np.max(sample) < boundaries[1]:
-                new_.append(n_s)
-        new = new_
-
-    if new:
-        selected.update(new)
-        shown.update(new)
-        return selected, shown
-    else:
-        return selected, shown
+        self.fig.canvas.draw()
