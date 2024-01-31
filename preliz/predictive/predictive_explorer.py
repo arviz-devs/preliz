4695641538e1ef3efbfc61d24621e78581c6f153
Osvaldo A Martin <aloctavodia@gmail.com>
2024-01-31 16:52:18 -0300

    add plot_func kwarg (#307)

--- preliz/predictive/predictive_explorer.py
+++ preliz/predictive/predictive_explorer.py
@@ -8,7 +8,7 @@ from preliz.internal.parser import inspect_source, parse_function_for_pred_textb
 from preliz.internal.plot_helper import get_textboxes, plot_decorator
 
 
-def predictive_explorer(fmodel, samples=50, kind_plot="ecdf", references=None):
+def predictive_explorer(fmodel, samples=50, kind_plot="ecdf", references=None, plot_func=None):
     """
     Create textboxes and plot a set of samples returned by a function relating one or more
     PreliZ distributions.
@@ -24,21 +24,20 @@ def predictive_explorer(fmodel, samples=50, kind_plot="ecdf", references=None):
         The number of samples to draw from the prior predictive distribution (default is 50).
     kind_plot : str, optional
         The type of plot to display. Defaults to "kde". Options are "hist" (histogram),
-        "kde" (kernel density estimate), "ecdf" (empirical cumulative distribution function),
-        or None (no plot).
+        "kde" (kernel density estimate), "ecdf" (empirical cumulative distribution function).
     references : int, float, list, tuple or dictionary
         Value(s) used as reference points representing prior knowledge. For example expected
         values or values that are considered extreme. Use a dictionary for labeled references.
+    plot_func : function
+        Custom matplotlib code. Defaults to None. ``kind_plot`` and ``references`` are ignored
+        if ``plot_func`` is specified.
     """
     source, signature = inspect_source(fmodel)
 
     model = parse_function_for_pred_textboxes(source, signature)
     textboxes = get_textboxes(signature, model)
 
-    if kind_plot is None:
-        new_fmodel = fmodel
-    else:
-        new_fmodel = plot_decorator(fmodel, samples, kind_plot, references)
+    new_fmodel = plot_decorator(fmodel, samples, kind_plot, references, plot_func)
 
     out = interactive_output(new_fmodel, textboxes)
     default_names = ["__set_xlim__", "__x_min__", "__x_max__", "__resample__"]
