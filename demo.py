84e35966567a3c9de6e61d263390b8cd4510c4be
Nishant Rajadhyaksha <79035403+nishant42491@users.noreply.github.com>
2024-02-08 19:10:17 +0530

    Update demo.py
    Co-authored-by: Osvaldo A Martin <aloctavodia@gmail.com>

--- demo.py
+++ demo.py
@@ -1,94 +1,37 @@
-from scipy.stats import beta
-from typing import List
-from scipy.optimize import bisect
+import preliz as pz
 
+def calc_cdf(l1, u1, a, b):
+    dist = pz.Beta(a, b)
+    return dist.cdf(u1) - dist.cdf(l1)
 
-def calc_cdf(r: List[float], a: float, b: float):
-
-    cdf1 = beta.cdf(r[0], a, b)
-    cdf2 = beta.cdf(r[1], a, b)
-
-    return cdf2-cdf1
-
-def tau_func(tau_not):
-
-    l1 = 0.25
-    u1 = 0.75
-
-    stop_prob = 0.99
-
-    mode = (l1 + u1) / 2
-
-    alpha1 = 1 + mode * tau_not
-    alpha2 = 1 + (1 - mode) * tau_not
-
-    prob = calc_cdf([l1, u1], alpha1, alpha2)
-
-    return prob-stop_prob
-
-
-def calc_beta_from_tau(tau_not, l1, u1):
-
-        mode = (l1 + u1) / 2
-
-        alpha1 = 1 + mode * tau_not
-        alpha2 = 1 + (1 - mode) * tau_not
-
-        prob = calc_cdf([l1, u1], alpha1, alpha2)
-
-        return prob
-
-
-def one_iter(tau_not, l1, u1, stop_prob):
-
-
-    mode = (l1 + u1) / 2
-
-    alpha1 = 1 + mode * tau_not
-    alpha2 = 1 + (1 - mode) * tau_not
-
-    prob = calc_cdf([l1, u1], alpha1, alpha2)
-
-    while prob < stop_prob:
-
-        tau_not = tau_not + 4
-
-        alpha1 = 1 + mode * tau_not
-        alpha2 = 1 + (1 - mode) * tau_not
-
-        prob = calc_cdf([l1, u1], alpha1, alpha2)
-
-    return tau_not
-
-
-
-if __name__ == '__main__':
-
-    l1 = 0.25
-    l2 = 0.75
-
-    eps = 0.005
-
-    stop_prob = 0.99
-
-    mode = 0.5
+def one_iter(l1, u1, stop_prob=0.99, eps=0.001):
 
     tau_not = 0
+    mode = (l1 + u1) / 2
 
-    cur_prob = calc_beta_from_tau(tau_not,l1,l2)
+    prob = calc_cdf(l1, u1, 1, 1)
+    
+    while abs(prob - stop_prob) > eps:
 
-    tau_star = one_iter(tau_not, l1, l2, 0.99)
+        tau_not +=  0.1
 
-    tau_bisected = bisect(tau_func, tau_not, tau_star)
+        alpha1 = 1 + mode * tau_not
+        alpha2 = 1 + (1 - mode) * tau_not
 
-    aplha1 = 1 + mode * tau_bisected
-    alpha2 = 1 + (1 - mode) * tau_bisected
+        prob =  calc_cdf(l1, u1, alpha1, alpha2)
 
-    cur_prob = calc_cdf([l1, l2], aplha1, alpha2)
+    return alpha1, alpha2, prob
 
-    print("tau_star: ", tau_star)
-    print("tau_bisected: ", tau_bisected)
-    print("alpha1: ", aplha1)
-    print("alpha2: ", alpha2)
-    print("cur_prob: ", cur_prob)
 
+
+l1 = 0.25
+l2 = 0.75
+mode = (l1 + l2) / 2
+alpha1, alpha2, cur_prob = one_iter(l1, l2)
+
+
+print("alpha1: ", alpha1)
+print("alpha2: ", alpha2)
+print("cur_prob: ", cur_prob)
+pz.Beta(alpha1, alpha2).plot_pdf()
+pz.maxent(pz.Beta(), l1, l2, 0.99);
