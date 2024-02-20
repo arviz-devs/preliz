a7df9f09648c4ff43b7ea097a4283b0333165e84
nishant <n.rajadhyaksha@somaiya.edu>
2024-02-20 23:53:27 +0530

    fixes for pylint

--- preliz/tests/test_beta_mode.py
+++ preliz/tests/test_beta_mode.py
@@ -13,7 +13,6 @@ from preliz import beta_mode
 def test_beta_mode(lower, upper, mode, mass, expected_alpha, expected_beta):
     _, dist = beta_mode(lower, upper, mode, mass)
 
-    # Assert that alpha and beta parameters of the fitted Beta distribution are close to expected values
     assert np.isclose(dist.alpha, expected_alpha, atol=0.01)
     assert np.isclose(dist.beta, expected_beta, atol=0.01)
 
@@ -34,6 +33,5 @@ def test_invalid_bounds():
 
 
 def test_plot_beta_mode():
-    _, dist = beta_mode(0, 1, 0.5, 0.9, plot=True,
+    _, _ = beta_mode(0, 1, 0.5, 0.9, plot=True,
                         plot_kwargs={"pointinterval": True})
-
