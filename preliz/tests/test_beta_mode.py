cda9df490011cfe927dd272f39046398efcf04f7
nishant <n.rajadhyaksha@somaiya.edu>
2024-02-21 16:49:14 +0530

    direct function call

--- preliz/tests/test_beta_mode.py
+++ preliz/tests/test_beta_mode.py
@@ -5,12 +5,10 @@ from preliz import beta_mode
 
 def test_beta_mode():
 
-    lower, upper, mode, mass, expected_alpha, expected_beta = 0.25, 0.75, 0.5, 0.9, 4.94, 4.94
+    _, dist = beta_mode(0.25, 0.75, 0.5, 0.9)
 
-    _, dist = beta_mode(lower, upper, mode, mass)
-
-    assert np.isclose(dist.alpha, expected_alpha, atol=0.01)
-    assert np.isclose(dist.beta, expected_beta, atol=0.01)
+    assert np.isclose(dist.alpha, 4.94, atol=0.01)
+    assert np.isclose(dist.beta, 4.94, atol=0.01)
 
 
 def test_invalid_mass():
