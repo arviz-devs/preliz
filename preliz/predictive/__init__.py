380bd0f46bc653f06e390526ecc6ae1a52956be0
aloctavodia <aloctavodia@gmail.com>
2024-02-26 11:40:30 -0300

    dpf

--- preliz/predictive/__init__.py
+++ preliz/predictive/__init__.py
@@ -1,6 +1,5 @@
 from .ppa import ppa
 from .predictive_explorer import predictive_explorer
-from .predictive_finder import predictive_finder
 
 
-__all__ = ["ppa", "predictive_explorer", "predictive_finder"]
+__all__ = ["ppa", "predictive_explorer"]
