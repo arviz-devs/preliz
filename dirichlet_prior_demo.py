035f3dfcd7b6e3c0126ac69ff12166303e8a7076
nishant <n.rajadhyaksha@somaiya.edu>
2024-02-28 22:32:48 +0530

    changed docs

--- dirichlet_prior_demo.py
+++ dirichlet_prior_demo.py
@@ -33,7 +33,7 @@ def dirichlet_mode(mode, mass=0.90, bound=0.01, plot=True, plot_kwargs={}, ax=No
         Mode of the Dirichlet distribution.
 
     mass : float
-        Probability mass between ``lower`` and ``upper`` bounds. Defaults to 0.94.
+        Probability mass between ``lower`` and ``upper`` bounds. Defaults to 0.90.
 
     bound : float
         Defines upper and lower bounds for the mass as mode+-bound. Defaults to 0.01.
