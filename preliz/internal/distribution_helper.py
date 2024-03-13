c264ff5135ae9b727544390750b016a802b75a5c
aloctavodia <aloctavodia@gmail.com>
2024-03-13 10:12:53 -0300

    Add faster Weibull

--- preliz/internal/distribution_helper.py
+++ preliz/internal/distribution_helper.py
@@ -1,7 +1,5 @@
 import re
 import numpy as np
-from scipy.special import gamma
-
 
 eps = np.finfo(float).eps
 
@@ -41,27 +39,6 @@ def hdi_from_pdf(dist, mass=0.94):
         if mass_cum >= mass:
             break
     return x_vals[np.sort(indices)[[0, -1]]]
-
-
-def garcia_approximation(mean, sigma):
-    """
-    Approximate method of moments for Weibull distribution, provides good results for values of
-    alpha larger than 0.83.
-
-    Oscar Garcia. Simplified method-of-moments estimation for the Weibull distribution. 1981.
-    New Zealand Journal of Forestry Science 11:304-306
-    https://www.scionresearch.com/__data/assets/pdf_file/0010/36874/NZJFS1131981GARCIA304-306.pdf
-    """
-    ks = [-0.221016417, 0.010060668, 0.117358987, -0.050999126]  # pylint: disable=invalid-name
-    z = sigma / mean  # pylint: disable=invalid-name
-
-    poly = 0
-    for idx, k in enumerate(ks):
-        poly += k * z**idx
-
-    alpha = 1 / (z * (1 + (1 - z) ** 2 * poly))
-    beta = 1 / (gamma(1 + 1 / alpha) / (mean))
-    return alpha, beta
 
 
 def all_not_none(*args):
