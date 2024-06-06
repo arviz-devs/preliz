c07af77aedac38fd4005c229075adf2d0d79f09e
aloctavodia <aloctavodia@gmail.com>
2024-06-06 14:13:51 -0300

    further changes

--- preliz/unidimensional/dirichlet_mode.py
+++ preliz/unidimensional/dirichlet_mode.py
@@ -13,29 +13,29 @@ def dirichlet_mode(mode, mass=0.90, bound=0.01, plot=True, plot_kwargs=None, ax=
     Returns a Dirichlet distribution where the marginals have the specified mode
     and mass and their masses lie within the range mode ± bound.
 
-     Parameters
-     ----------
-     mode : list
-         Mode of the Dirichlet distribution.
-     mass : float
-         Probability mass between within mode +- bounds. Defaults to 0.90.
-     bound : float
-         Defines upper and lower bounds for the mass as mode +- bound. Defaults to 0.01.
-     plot : bool
-         Whether to plot the distribution. Defaults to True.
-     plot_kwargs : dict
-         Dictionary passed to the method ``plot_pdf()``.
-     ax : matplotlib axes
+    Parameters
+    ----------
+    mode : list
+        Mode of the Dirichlet distribution.
+    mass : float
+        Probability mass between within mode +- bounds. Defaults to 0.90.
+    bound : float
+        Defines upper and lower bounds for the mass as mode +- bound. Defaults to 0.01.
+    plot : bool
+        Whether to plot the distribution. Defaults to True.
+    plot_kwargs : dict
+        Dictionary passed to the method ``plot_pdf()``.
+    ax : matplotlib axes
 
-     Returns
-     -------
-     ax : matplotlib axes
-     dist : Preliz Dirichlet distribution.
-         Dirichlet distribution with fitted parameters alpha for the given mass and intervals.
+    Returns
+    -------
+    ax : matplotlib axes
+    dist : Preliz Dirichlet distribution.
+        Dirichlet distribution with fitted parameters alpha for the given mass and intervals.
 
-     References
-     ----------
-     Adapted from  Evans et al. (2017) see https://doi.org/10.3390/e19100564
+    References
+    ----------
+    Adapted from  Evans et al. (2017) see https://doi.org/10.3390/e19100564
     """
 
     if not 0 < mass <= 1:
