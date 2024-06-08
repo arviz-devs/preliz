de5341b408bcfaa98c2ab3b47f0f3e036b8f6f9c
aloctavodia <aloctavodia@gmail.com>
2024-06-08 17:18:56 -0300

    use g format string for distribution repr

--- preliz/distributions/distributions.py
+++ preliz/distributions/distributions.py
@@ -46,8 +46,11 @@ class Distribution:
             bolded_name = "\033[1m" + name + "\033[0m"
 
             description = "".join(
-                f"{n}={np.round(v, 2)}," for n, v in zip(self.param_names, self.params)
-            ).strip(",")
+                f"{n}={v:.3g}, "
+                if np.isscalar(v)
+                else f"{n}=[" + "".join(f"{vi:.3g}, " for vi in v).strip(", ") + "], "
+                for n, v in zip(self.param_names, self.params)
+            ).strip(", ")
 
             return f"{bolded_name}({description})"
         else:
