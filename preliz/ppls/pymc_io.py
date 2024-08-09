190de5dcd3727ed3ad4bc11fc06ebc52360aa962
aloctavodia <aloctavodia@gmail.com>
2024-08-09 19:48:46 -0300

    refactor

--- preliz/ppls/pymc_io.py
+++ preliz/ppls/pymc_io.py
@@ -213,40 +213,44 @@ def non_constant_parents(var_, free_rvs):
 
 def posterior_to_prior(model, posterior, alternative=None):
     """
-    Updates the priors of a probabilistic model by fitting them to posterior data, using either predefined or
-    user-specified alternative distributions. It selects the best-fitting distribution for each variable based
-    on maximum likelihood estimation (MLE). The result is a model with priors better aligned to the observed data.
+    Fit a posterior from a model to its prior
+
+    The fit is based on maximum likelihood of each posterior marginal to the prior
+    in the model. It is expected that the posterior was computed from the model.
 
     Parameters
     ----------
     model : A PyMC model
-    A probabilistic model
-
-    posterior : Posterior samples
-    InferenceData from with the posterior group
-
-    alternative : "auto", list, dict, defaults to None
-    Users can add the model variables to consider alternative distributions while fitting samples
-
+    posterior : InferenceData
+        InferenceData with a posterior group.
+    alternative : "auto", list or dict
+        Defaults to None, the samples are fit to the original prior distribution.
+        If "auto", the method evaluates the fit to the original prior plus a set of
+        predefined distributions.
+        Use a list of PreliZ distribution to specify the alternative distributions
+        you want to consider.
+        Use a dict with variables names in ``model`` as key and a list of preliz
+        distributions as value. This allows to specify alternative distributions
+        per variable.
     """
-
     model_info = get_model_information(model)[2]
-    parsed_info = [(dist, var) for var, dist in model_info.items()]
     new_priors = []
 
-    for dist, var in parsed_info:
-        dists = [model_info[var]]
+    if alternative is None:
+        for var, dist in model_info.items():
+            dists._fit_mle(posterior[var].values)
+            new_priors.append((dist, var))
+    else:
+        for var, dist in model_info.items():
+            dists = [dist]
 
-        if alternative == "auto":
-            dists += [Normal(), HalfNormal(), Gamma()]
-        elif isinstance(alternative, list):
-            dists += alternative
-        elif isinstance(alternative, dict):
-            dists += alternative.get(var, [])
-        if len(dists) == 1:
-            dists[0]._fit_mle(posterior[var].values)
-            new_priors.append((dists[0], var))
-        else:
+            if alternative == "auto":
+                dists += [Normal(), HalfNormal(), Gamma()]
+            elif isinstance(alternative, list):
+                dists += alternative
+            elif isinstance(alternative, dict):
+                dists += alternative.get(var, [])
+
             idx = mle(dists, posterior[var].values, plot=False)[0]
             new_priors.append((dists[idx[0]], var))
 
