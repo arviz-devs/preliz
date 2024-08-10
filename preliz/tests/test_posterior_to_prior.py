71077ac5afce12dce5a9b4945479a1788d9fafe7
aloctavodia <aloctavodia@gmail.com>
2024-08-10 18:29:48 -0300

    add basic test

---
+++ preliz/tests/test_posterior_to_prior.py
@@ -0,0 +1,17 @@
+import preliz as pz
+import pymc as pm
+
+data = pz.Normal(0, 1).rvs(200)
+
+with pm.Model() as model:
+    a = pm.Normal("a", mu=0, sigma=1)
+    b = pm.HalfNormal("b", sigma=1)
+    y = pm.Normal("y", mu=a, sigma=b, observed=data)
+    idata = pm.sample(tune=200, draws=200)
+
+
+def test_p2p():
+    pz.posterior_to_prior(model, idata)
+    pz.posterior_to_prior(model, idata, alternative="auto")
+    pz.posterior_to_prior(model, idata, alternative=[pz.LogNormal()])
+    pz.posterior_to_prior(model, idata, alternative={"b": [pz.Gamma(mu=0)]})
