034372e5c2cfa980980270eb0f1e4126f149cb75
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2024-08-13 23:57:33 +0530

    Update test

--- preliz/tests/test_posterior_to_prior.py
+++ preliz/tests/test_posterior_to_prior.py
@@ -14,10 +14,8 @@ with pm.Model() as model:
 
 def test_p2p():
     pz.posterior_to_prior(model, idata)
-    pattern = r'\s*b = pm.\x1b\[1mGamma\x1b\[0m\("b",\s*alpha\s*=\s*([+-]?\d*\.?\d+),\s*beta\s*=\s*([+-]?\d*\.?\d+)\)'
-    assert bool(re.search(pattern, pz.posterior_to_prior(model, idata, alternative="auto")))
+    assert 'Gamma\x1b[0m("b", alpha=' in pz.posterior_to_prior(model, idata, alternative="auto")
     pz.posterior_to_prior(model, idata, alternative=[pz.LogNormal()])
-    pattern = r'\s*b = pm.\x1b\[1mGamma\x1b\[0m\("b",\s*mu\s*=\s*([+-]?\d*\.?\d+),\s*sigma\s*=\s*([+-]?\d*\.?\d+)\)'
-    assert bool(
-        re.search(pattern, pz.posterior_to_prior(model, idata, alternative={"b": [pz.Gamma(mu=0)]}))
+    assert 'Gamma\x1b[0m("b", mu=' in pz.posterior_to_prior(
+        model, idata, alternative={"b": [pz.Gamma(mu=0)]}
     )
