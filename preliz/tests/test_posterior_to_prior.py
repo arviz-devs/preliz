6b1f16e1aa2a44468de968d3b02abd9d04dab7a9
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2024-08-13 10:38:00 +0530

    Update tests for posterior_to_prior

--- preliz/tests/test_posterior_to_prior.py
+++ preliz/tests/test_posterior_to_prior.py
@@ -1,3 +1,5 @@
+import re
+
 import preliz as pz
 import pymc as pm
 
@@ -12,6 +14,10 @@ with pm.Model() as model:
 
 def test_p2p():
     pz.posterior_to_prior(model, idata)
-    pz.posterior_to_prior(model, idata, alternative="auto")
+    pattern = r'\s*b = pm.\x1b\[1mGamma\x1b\[0m\("b",\s*alpha\s*=\s*([+-]?\d*\.?\d+),\s*beta\s*=\s*([+-]?\d*\.?\d+)\)'
+    assert bool(re.search(pattern, pz.posterior_to_prior(model, idata, alternative="auto")))
     pz.posterior_to_prior(model, idata, alternative=[pz.LogNormal()])
-    pz.posterior_to_prior(model, idata, alternative={"b": [pz.Gamma(mu=0)]})
+    pattern = r'\s*b = pm.\x1b\[1mGamma\x1b\[0m\("b",\s*mu\s*=\s*([+-]?\d*\.?\d+),\s*sigma\s*=\s*([+-]?\d*\.?\d+)\)'
+    assert bool(
+        re.search(pattern, pz.posterior_to_prior(model, idata, alternative={"b": [pz.Gamma(mu=0)]}))
+    )
