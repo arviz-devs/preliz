41faaf4b12572fe537fecee517f87d13e1c4e924
rohanbabbar04 <rohanbabbar0408@yahoo.com>
2024-08-15 11:58:37 +0530

    Update bambi test

--- preliz/ppls/agnostic.py
+++ preliz/ppls/agnostic.py
@@ -1,11 +1,17 @@
 """Functions to communicate with PPLs."""
 
+import logging
+
+from preliz.internal.parser import get_engine
 from preliz.distributions import Gamma, Normal, HalfNormal
 from preliz.unidimensional.mle import mle
 from preliz.ppls.pymc_io import get_model_information, write_pymc_string
+from preliz.ppls.bambi_io import get_bmb_model_information, write_bambi_string
+
+_log = logging.getLogger("preliz")
 
 
-def posterior_to_prior(model, idata, alternative=None):
+def posterior_to_prior(model, idata, alternative=None, engine="auto"):
     """
     Fit a posterior from a model to its prior
 
@@ -14,7 +20,7 @@ def posterior_to_prior(model, idata, alternative=None):
 
     Parameters
     ----------
-    model : A PyMC model
+    model : A PyMC or a Bambi Model
     idata : InferenceData
         InferenceData with a posterior group.
     alternative : "auto", list or dict
@@ -26,8 +32,17 @@ def posterior_to_prior(model, idata, alternative=None):
         Use a dict with variables names in ``model`` as keys and a list of PreliZ
         distributions as values. This allows to specify alternative distributions
         per variable.
+    engine : "auto", "pymc" or "bambi"
+        Library used to define the model. Either `pymc` or `bambi`. Default is `auto`.
+        The function will automatically select the appropriate library to use based on the model
+        provided.
     """
-    _, _, model_info, _, var_info2, *_ = get_model_information(model)
+    _log.info(""""This is an experimental method under development, use with caution.""")
+    engine = get_engine(model) if engine == "auto" else engine
+    if engine == "bambi":
+        _, _, model_info, _, var_info2, *_ = get_bmb_model_information(model)
+    elif engine == "pymc":
+        _, _, model_info, _, var_info2, *_ = get_model_information(model)
     new_priors = {}
     posterior = idata.posterior.stack(sample=("chain", "draw"))
 
@@ -48,7 +63,9 @@ def posterior_to_prior(model, idata, alternative=None):
 
             idx, _ = mle(dists, posterior[var].values, plot=False)
             new_priors[var] = dists[idx[0]]
-
-    new_model = write_pymc_string(new_priors, var_info2)
+    if engine == "bambi":
+        new_model = write_bambi_string(new_priors, var_info2)
+    elif engine == "pymc":
+        new_model = write_pymc_string(new_priors, var_info2)
 
     return new_model
