0a3ba3f72964c1f577b71958177de65228aa594c
Osvaldo A Martin <aloctavodia@gmail.com>
2025-01-08 01:19:28 -0300

    Improve interactive tests (#623)
    * improve tests
    
    * remove print
    
    * update notebook
    
    * remove test

--- preliz/tests/test_helper.py
+++ preliz/tests/test_helper.py
@@ -6,10 +6,22 @@ from nbconvert.preprocessors import ExecutePreprocessor
 def run_notebook(notebook):
     current_dir = path.dirname(path.realpath(__file__))
     file_path = path.join(current_dir, notebook)
-    with open(file_path, encoding="ascii") as inb_f:
+
+    with open(file_path, encoding="utf-8") as inb_f:
         inb = nbformat.read(inb_f, as_version=4)
-        exec_pre = ExecutePreprocessor(timeout=600, kernel_name="python3")
-        try:
-            assert exec_pre.preprocess(inb) is not None, f"Got empty notebook for {notebook}"
-        except Exception:  # pylint: disable=broad-except
-            assert False, f"Failed executing {notebook}"
+
+    exec_pre = ExecutePreprocessor(timeout=600, kernel_name="python3")
+
+    try:
+        exec_pre.preprocess(inb, {"metadata": {"path": current_dir}})
+    except Exception as e:
+        raise RuntimeError(f"Notebook execution failed: {e}") from e
+
+    for cell in inb.cells:
+        if cell.cell_type == "code" and "outputs" in cell:
+            for output in cell.outputs:
+                try:
+                    if "FAILED" in output.text:
+                        raise RuntimeError(f"Error in notebook cell:\n{output.text}")
+                except AttributeError:
+                    pass
