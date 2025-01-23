5e8047cdebb003f5db4dd1024a107ee885d6299d
aleicazatti <ale.icazatti@gmail.com>
2025-01-23 14:01:56 -0300

    Distributions Gallery: Separate Colors by Category

--- docs/get_cover_gallery.py
+++ docs/get_cover_gallery.py
@@ -7,11 +7,70 @@ from preliz.internal.distribution_helper import init_vals
 style.use("preliz-doc")
 rng = np.random.default_rng(247)
 
+category_colors = {
+    'continuous': 'C3',
+    'discrete': 'C1',
+    'modifier': 'C2'
+}
+
+distribution_categories = {
+    'AsymmetricLaplace': 'continuous',
+    'Beta': 'continuous',
+    'BetaScaled': 'continuous',
+    'Cauchy': 'continuous',
+    'Censored': 'modifier',
+    'ChiSquared': 'continuous',
+    'ExGaussian': 'continuous',
+    'Exponential': 'continuous',
+    'Gamma': 'continuous',
+    'Gumbel': 'continuous',
+    'HalfCauchy': 'continuous',
+    'HalfNormal': 'continuous',
+    'HalfStudentT': 'continuous',
+    'InverseGamma': 'continuous',
+    'Kumaraswamy': 'continuous',
+    'Laplace': 'continuous',
+    'LogNormal': 'continuous',
+    'Logistic': 'continuous',
+    'LogLogistic': 'continuous',
+    'LogitNormal': 'continuous',
+    'Moyal': 'continuous',
+    'Normal': 'continuous',
+    'Pareto': 'continuous',
+    'Rice': 'continuous',
+    'SkewNormal': 'continuous',
+    'StudentT': 'continuous',
+    'SkewStudentT': 'continuous',
+    'Triangular': 'continuous',
+    'Truncated': 'modifier',
+    'TruncatedNormal': 'continuous',
+    'Uniform': 'continuous',
+    'VonMises': 'continuous',
+    'Wald': 'continuous',
+    'Weibull': 'continuous',
+    'Bernoulli': 'discrete',
+    'BetaBinomial': 'discrete',
+    'Binomial': 'discrete',
+    'Categorical': 'discrete',
+    'DiscreteUniform': 'discrete',
+    'DiscreteWeibull': 'discrete',
+    'Geometric': 'discrete',
+    'HyperGeometric': 'discrete',
+    'NegativeBinomial': 'discrete',
+    'Poisson': 'discrete',
+    'ZeroInflatedBinomial': 'discrete',
+    'ZeroInflatedNegativeBinomial': 'discrete',
+    'ZeroInflatedPoisson': 'discrete',
+    'Mixture': 'modifier',
+    'Hurdle': 'modifier',
+}
+
 init_vals["Hurdle"] = None
 init_vals["Mixture"] = None
 init_vals["SkewStudentT"] = {"mu": 0.0, "sigma": 1, "a": 2.5, "b": 1.5}
 for name, params in init_vals.items():
-    color = f"C{rng.integers(0, 4)}"
+    category = distribution_categories.get(name, 'continuous')
+    color = category_colors.get(category, 'black')
     _, ax = plt.subplots(figsize=(3.5, 2.3))
     dist = getattr(distributions, name)
     if name in ["Truncated", "Censored"]:
