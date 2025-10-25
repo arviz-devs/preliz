dd15ab6eaf0b35f16a8e9bb92d6b870c98a1173a
aloctavodia <aloctavodia@gmail.com>
2025-10-25 21:09:53 +0300

    add docstring

--- preliz/distributions/plot.py
+++ preliz/distributions/plot.py
@@ -15,6 +15,52 @@ def plot(
     ax=None,
     **kwargs,
 ):
+    """
+    Plot distribution functions, including pdf, cdf, ppf, sf and isf.
+
+    Parameters
+    ----------
+    kind : str
+        Specify which distribution function to plot. Defaults to ``pdf``, i.e. the probability
+        density function for continuos distributions or probability mass function for discrete
+        distributions.
+        Others possible values are:
+        * ``cdf`` cumulative distribution function
+        * ``ppf`` percent point function, also know as inverse of the cdf
+        * ``sf`` survival function (1-cdf)
+        * ``isf`` inverse survival function
+    moments : str
+        Compute moments. Use any combination of the strings ``m``, ``d``, ``v``, ``s`` or ``k``
+        for the mean (μ), standard deviation (σ), variance (σ²), skew (γ) or kurtosis (κ)
+        respectively. Other strings will be ignored. Defaults to None.
+    pointinterval : bool
+        Whether to include a plot of the quantiles. Defaults to False. If True the default is to
+        plot the median and two interquantiles ranges.
+    interval : str
+        Type of interval. Available options are highest density interval `"hdi"` (default),
+        equal tailed interval `"eti"` or intervals defined by arbitrary `"quantiles"`.
+        Defaults to the value in rcParams["stats.ci_kind"].
+    levels : list
+        Mass of the intervals. For hdi or eti the number of elements should be 2 or 1.
+        For quantiles the number of elements should be 5, 3, 1 or 0
+        (in this last case nothing will be plotted).
+    support : str:
+        If ``full`` use the finite end-points to set the limits of the plot. For unbounded
+        end-points or if ``restricted`` use the 0.001 and 0.999 quantiles to set the limits.
+        Ignored when ``kind="ppf"``, ``kind="isf"``.
+    baseline : bool
+        Whether to include a horizontal line at y=0. Only used when ``kind="pdf"``, ignored
+        otherwise.
+    legend : str
+        Whether to include a string with the distribution and its parameter as a ``"legend"`` a
+        ``"title"`` or not include them ``None``.
+    figsize : tuple
+        Size of the figure
+    ax : matplotlib axes
+    kwargs : keyword arguments
+        Additional keyword arguments passed to matplotlib plot function.
+        For example, ``color``, ``alpha``, ``linewidth``, etc.
+    """
     if kind not in ["pdf", "cdf", "ppf", "sf", "isf"]:
         raise ValueError(f"kind should be one of {['pdf', 'cdf', 'ppf', 'sf', 'isf']}")
 
